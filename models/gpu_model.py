bba3c6334f9ff509450762ebdae5fae3acb881d8
justinfinn2891 <jfinn5@ut.utm.edu>
2025-05-18 19:16:51 -0500

    recurring neural network model added

--- models/gpu_model.py
+++ models/gpu_model.py
@@ -3,7 +3,27 @@
 import torch.nn as nn 
 import torch.nn.functional as F 
 
-class model(nn.Module):
-    def __init__(self):
-        super(model, self).__init__()
-         
+#creating a summary of the previous days into a fixed size vector and use that to predict ayear
+#https://www.youtube.com/watch?v=YUGbMdfgpx0
+class modelForPredictions(nn.Module):
+    def __init__(self, input_size=1, hidden_size=64, num_layers=2, output_size=1):
+        super(modelForPredictions, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, output_size)
+        )
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])  # Use output from last timestep
+        return out
